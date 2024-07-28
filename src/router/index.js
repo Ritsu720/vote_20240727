5c83c3fe16f2d046853032adf34363bf3d706401
Ritsu720 <julie890909@gmail.com>
2024-07-28 13:23:27 +0900

    bugs

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+VueElement.use(Router);
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+
+    ]
+})
---
+++ src/views/AddVote.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+
+<template>
+<p>hello</p>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
